76939ba5ab374e6b2f9921cfbcd287a911f641fd
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-11-06 13:50:54 -0300

    feat: initial commit

---
+++ api/controller/itemController.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Item = require("../models/Item");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const item = await Item.create(req.body);
+    return res.status(200).send({ item });
+  } catch (err) {
+    return res.status(400).send({
+      err: "Não foi possível adicionar o item ao banco",
+    });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const item = await Item.find();
+    return res.status(200).send({ item });
+  } catch (err) {
+    return res.status(400).send({
+      err: "Não foi possível retornar nenhum item do banco",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    return res
+      .status(200)
+      .send({ message: "Item removido do banco", log: item });
+  } catch (err) {
+    return res
+      .status(404)
+      .send({ err: "Não foi possível concluir a operação", log: req.body });
+  }
+});
+
+module.exports = (app) => app.use("/storage", router);
---
+++ api/models/Item.js
@@ -0,0 +1,27 @@
+const mongoose = require("../database");
+
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+module.exports = Item;
