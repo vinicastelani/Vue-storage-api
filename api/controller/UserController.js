c2e79aa826b76296981c0c8288379ea2b6d7be44
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-11-10 22:13:47 -0300

    feat: added login functions

---
+++ api/controller/UserController.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.json");
+const User = require("../models/User");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { email } = req.body;
+  try {
+    if (await User.findOne({ email }))
+      return res.status(404).send({
+        msg: { type: "error", data: "Este email já está em uso." },
+      });
+
+    const user = await User.create(req.body);
+    user.password = undefined;
+    return res.status(200).send({
+      msg: { type: "success", data: "Usuário criado" },
+      log: user,
+    });
+  } catch (msg) {
+    return res.status(404).send({
+      msg: { type: "error", data: "Não foi posssível criar o usuário" },
+      log: req.body,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).send({
+        msg: { type: "error", data: "Usuário não encontrado" },
+      });
+    }
+
+    if (!(await bcrypt.compare(password, user.password)))
+      return res.status(400).send({
+        msg: { type: "error", data: "Senha inválida" },
+      });
+    const token = jwt.sign({ id: user._id }, config.secret);
+    user.password = undefined;
+    return res.status(200).send({
+      msg: { type: "success", data: "Logando..." },
+      log: user,
+      token,
+    });
+  } catch (msg) {
+    return res.status(404).send({
+      msg: { type: "error", data: "Não foi possível concluir a operação" },
+    });
+  }
+});
+
+module.exports = (app) => app.use("/user", router);
--- api/index.js
+++ api/index.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 require("./controller/itemController")(app);
+require("./controller/UserController")(app);
 
 app.listen(process.env.PORT || 4000, () =>
   console.log(`server listening at port ${process.env.PORT || 4000}`)
