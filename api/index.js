c6c0250abdeac49c9cd2f7808986f2181746caa1
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-11-10 23:13:08 -0300

    fix: fix cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 require("./controller/itemController")(app);
