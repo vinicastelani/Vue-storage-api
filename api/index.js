92ec22bf41682d0a9b3f44521bd7e86fa0bbdb87
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-12-01 14:52:57 -0300

    fix: node engine

--- api/index.js
+++ api/index.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 require("./controller/itemController")(app);
 require("./controller/UserController")(app);
 require("./controller/CatalogController")(app);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(process.env.PORT || 4000, () =>
   console.log(`server listening at port ${process.env.PORT || 4000}`)
